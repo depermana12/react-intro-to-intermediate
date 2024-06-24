b5e946a3c8e39047ce7821c803d38256646813c8
depermana12 <deddiapermana97@gmail.com>
2024-06-24 22:31:16 +0700

    first jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,44 @@
-import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
+
+import Pet from "./Pet";
 
 // information flowing from app into pet through "props"
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
-
 // one way data-flow, pass data from parent to child, but not the other way
 // if parent pass data to child, if there is an issue inside a parent(App in this case)
 // obviously child(pet in this case) did not cause it
 
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt me"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Kiko",
+//       animal: "Rabbit",
+//       breed: "Angora",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Sapi",
+//       animal: "Cat",
+//       breed: "Persian",
+//     }),
+//   ]);
+// };
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Kiko",
-      animal: "Rabbit",
-      breed: "Angora",
-    }),
-    React.createElement(Pet, {
-      name: "Sapi",
-      animal: "Cat",
-      breed: "Persian",
-    }),
-  ]);
+  return (
+    <>
+      <h1>Adopt me</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Kiko" animal="Rabbit" breed="Angora" />
+      <Pet name="Sapi" animal="Cat" breed="Orange" />
+    </>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
---
+++ src/Pet.jsx
@@ -0,0 +1,19 @@
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("h2", {}, props.animal),
+//     React.createElement("h2", {}, props.breed),
+//   ]);
+// };
+
+const Pet = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
+};
+
+export default Pet;
