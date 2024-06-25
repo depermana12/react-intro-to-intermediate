923fcb3057a4071016c6f20e328db1078c7f04ea
depermana12 <deddiapermana97@gmail.com>
2024-06-25 17:27:15 +0700

    basic client app routing, useParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt me</h1>
+      <header>
+        <Link to="/">Adopt me</Link>
+      </header>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
