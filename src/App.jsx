53a0076f632e317f4c43bf5f68db6d34c78e42c1
depermana12 <deddiapermana97@gmail.com>
2024-06-25 15:32:54 +0700

    client side router:react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <h1>Adopt me</h1>
-      <SearchParams />
-    </>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/Details.jsx
@@ -0,0 +1,4 @@
+const Details = () => {
+  return <h1>Hola</h1>;
+};
+export default Details;
