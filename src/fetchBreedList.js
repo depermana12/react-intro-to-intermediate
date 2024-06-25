74d186bcd8fad51464c1f900f1d4d4ea95b5062f
depermana12 <deddiapermana97@gmail.com>
2024-06-25 19:55:22 +0700

    refactor: using useQuery

---
+++ src/fetchBreedList.js
@@ -0,0 +1,15 @@
+const fetchBreedList = async ({ queryKey }) => {
+  const animal = queryKey[1];
+
+  if (!animal) return [];
+
+  const apiRes = await fetch(
+    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`,
+  );
+  if (!apiRes.ok) {
+    throw new Error(`details/${animal} fetch not ok`);
+  }
+  const data = apiRes.json();
+  return data;
+};
+export default fetchBreedList;
--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,35 +1,11 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
-const localCache = {};
-
-// custom hooks are basically just other hooks package together
-// encapsulate it, now this is testable, re-useable
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const { data, status } = useQuery({
+    queryKey: ["breeds", animal],
+    queryFn: fetchBreedList,
+  });
 
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      requestBreedList();
-    }
-
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading0");
-
-      const response = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`,
-      );
-
-      const data = await response.json();
-      localCache[animal] = data.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
-  return [breedList, status];
+  return [data?.breeds ?? [], status];
 }
