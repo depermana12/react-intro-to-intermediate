4bfaf7e14506ff49ac60972193c4aad6e098f441
depermana12 <deddiapermana97@gmail.com>
2024-06-28 11:15:27 +0700

    vitest

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -20,13 +20,19 @@ class Carousel extends Component {
     const { images } = this.props;
     return (
       <div className="mb-10 flex flex-col items-center justify-center">
-        <img src={images[active]} alt="animal" className="mb-4" />
+        <img
+          data-testid="hero"
+          src={images[active]}
+          alt="animal"
+          className="mb-4"
+        />
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5">
           {images.map((photo, index) => (
             // eslint-disable-next-line
             <img
               onClick={this.handleIndexClick}
               data-index={index}
+              data-testid={`thumbnail${index}`}
               key={photo}
               src={photo}
               className={`${index === active ? "active h-48 w-48 opacity-50" : "h-48 w-48"}`}
